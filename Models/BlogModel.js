e488740ec2eab6ace85632949a9deab2aed39db2
Aml Ibrahim (Mully) <amlmahdawy21@gmail.com>
2025-02-12 15:20:34 +0200

    user & blog models

---
+++ Models/BlogModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const BlogsSchema = mongoose.Schema({
+    title: {
+        type: String,
+        // minlength: 5,
+        // maxlength: 20,
+        required: true,
+
+    },
+    category: {
+        type: String,
+        minlength: 5,
+        // maxlength: 20,
+        required: true,
+
+    },
+    content: {
+        type: String,
+        // minlength: 30,
+        required: true,
+
+    },
+    userId: {
+        type: String,
+        required: true
+    }
+})
+
+function validateBlog(blog) {
+    const schema = joi.object({
+        title: joi.string().min(3).max(25).required(),
+        content: joi.string().required().email(),
+        category: joi.string().min(8).max(30).required()
+    })
+    return schema.validate(user);
+}
+module.exports = mongoose.model("Blog", BlogsSchema)
---
+++ Models/UserModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const joi = require('joi')
+const UsersSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 20,
+        set: v => v.toLowerCase().trim()
+    },
+    mail: {
+        type: String,
+        required: true,
+        set: v => v.toLowerCase().trim()
+
+    },
+    password: {
+        type: String,
+        minLength: 8
+    }
+
+})
+function validateUser(user) {
+    const schema = joi.object({
+        name: joi.string().min(3).max(25).required(),
+        mail: joi.string().required().email(),
+        password: joi.string().min(8).max(30).required()
+    })
+    return schema.validate(user);
+}
+exports.userModel = mongoose.model("User", UsersSchema);
+exports.validateUser = validateUser
