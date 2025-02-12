28e4b0933f7a64e1455203f1480c8aae5c1ad430
Aml Ibrahim (Mully) <amlmahdawy21@gmail.com>
2025-02-12 15:20:55 +0200

    blogs & user Routes

--- Routes/BlogsRoutes.js
+++ Routes/BlogsRoutes.js
@@ -1,9 +1,10 @@
-const express= require(express);
-const router=express.router();
-const BlogsController= require('../Controllers/BlogsController')
-router.get('/',BlogsController.getAllBlogs);
-router.post('/',BlogsController.addBlog);
-router.delete('/:id',BlogsController.deleteBlog);
-router.put("/:id",BlogsController.updateBlog);
+const express = require("express");
+const router = express.Router();
+const BlogsController = require('../Controllers/BlogsController');
+const authorise = require('../Middlewares/Authorise')
+router.get('/all', BlogsController.getAllBlogs);
+router.post('/create', BlogsController.addBlog);
+router.delete('/delete/:id', authorise, BlogsController.deleteBlog);
+router.put("/update/:id", authorise, BlogsController.updateBlog);
 
- module.exports=router;
+module.exports = router;
