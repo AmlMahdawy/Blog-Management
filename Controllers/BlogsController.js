5e1e5b64faeb253f0cc7c32c94c281693315baf8
Aml Ibrahim (Mully) <amlmahdawy21@gmail.com>
2025-02-12 17:14:46 +0200

    replacing authorise middleware to specific blog function

--- Controllers/BlogsController.js
+++ Controllers/BlogsController.js
@@ -1,25 +1,39 @@
 const blogsService = require('../services/blogs.service')
 
 const getAllBlogs = async (req, res, next) => {
-    const blogs = await blogsService.allBlogs(req, res, next)
+    const blogs = await blogsService.allBlogs()
     res.status(200).send(blogs)
 }
 
 const addBlog = async (req, res, next) => {
-
-    const blog = await blogsService.createBLog(req, res, next)
+    const blogData = { ...req.body }
+    const blog = await blogsService.createBLog(blogData)
     res.status(200).send(blog);
 }
 const deleteBlog = async (req, res, next) => {
-    const blog = await blogsService.deleteBlog(req, res, next)
-    res.status(200).send(blog);
+    const blogId = req.params.id;
+    const userId = req.body.userId;
+
+    const blog = await blogsService.blogById(blogId);
+    if (!blog) return res.status(404).send("Blog not found.");
+    if (blog.userId !== userId) return res.status(403).send("Access Denied");
+
+    const deletedBlog = await blogsService.deleteBlog(blogId);
+    res.status(200).send(deletedBlog);
 }
 
 const updateBlog = async (req, res, next) => {
+    const { title, content, category, userId } = req.body
+    const blogId = req.params.id;
 
-    const updatedBlog = await blogsService.updateBlog(req, res, next)
+    const blog = await blogsService.blogById(blogId);
+    if (!blog) return res.status(404).send("Blog not found.");
+    if (!blog.userId === userId) return res.status(403).send("Access Denied");
+    const updatedBlog = await blogsService.updateBlog(blogId, { title, content, category })
     res.status(200).send(updatedBlog)
 }
+
+
 
 module.exports = {
     getAllBlogs,
